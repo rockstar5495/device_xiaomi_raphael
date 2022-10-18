a2b2169b57118294d47dd9e78ab2d3c899273ede
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-10-18 13:00:11 +0530

    raphael: drop voltage stuff
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- device.mk
+++ device.mk
@@ -44,8 +44,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-voltage
+    $(LOCAL_PATH)/overlay
 
 # ANT+
 PRODUCT_PACKAGES += \
